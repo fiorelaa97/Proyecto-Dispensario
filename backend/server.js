6cfc66cb8b5ca0a7e284d5aa7bc62e1064ae3c01
Atina <atinamonges89@gmail.com>
2026-09-10 13:54:12 -0300

    codigo servidor

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
 const express = require("express")
+
+const app=express();
+const PORT =3000;
+
+app.use(express.json());
+
+app.listen(PORT, async() => {
+    console.log('servidor ejecutandose en http://localhost:${PORT')
+})
