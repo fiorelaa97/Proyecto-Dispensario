e7274b036340da091cfdbda893e47312c24926ed
Atina <atinamonges89@gmail.com>
2026-09-23 14:38:07 -0300

    cambio 2

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app=express();
 const PORT =3000;
 
-app.use(express.json());
+app.use(express.jsonqq());
 
 app.listen(PORT, async() => {
     console.log('servidor ejecutandose en http://localhost:${PORT')
