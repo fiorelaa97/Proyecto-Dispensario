09fa044a021a8663b5aa8497faac52cec56d8ec5
fiorelaa97 <godoyfiore1997@gmail.com>
2026-09-23 14:32:28 -0300

    ruta empleados

---
+++ backend/bd/conexion.js
@@ -0,0 +1,13 @@
+const mysql = require ("mysql2/promise");
+
+const pool = mysql.createPool({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1234",
+    database: "dispensario_bd"
+
+
+})
+
+module.exports = pool;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
-const express = require("express")
+const express = require("express");
+const pool = require("./bd/conexion")
 
 const app=express();
 const PORT =3000;
 
 app.use(express.json());
-
+async function probarConexion(){
+    try {
+        await pool.query("SELECT 1");
+        console.log("Conexion exitosa")
+    } catch (error) {
+        console.log("Error en la conexión")
+        console.log(error.menssage)
+        
+    }
+}
 app.listen(PORT, async() => {
-    console.log('servidor ejecutandose en http://localhost:${PORT')
+    console.log(`servidor ejecutandose en http://localhost:${PORT}`)
+    await probarConexion()
 })
